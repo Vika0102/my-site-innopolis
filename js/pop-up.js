fa2e5490bebea5c5eb3d998eb72bb3f5a78e58a1
Vika0102 <107152273+Vika0102@users.noreply.github.com>
2023-05-06 15:45:26 +0300

    Изменения

--- js/pop-up.js
+++ js/pop-up.js
@@ -1,10 +1,22 @@
 let news = document.querySelector('.news');
 let mailingPopUp = document.querySelector ('.mailing-pop-up');
+let Closenews = document.querySelector('.popup-close');
 
 news.addEventListener ('click', showPopup);
+Closenews.addEventListener ('click', close);
 
 function showPopup(evt) {
     evt.preventDefault();
 
     mailingPopUp.classList.toggle('show-popup');
 };
+
+function close() {
+    mailingPopUp.classList.remove('show-popup')
+}
+
+mailingPopUp.addEventListener('click', (e) => {
+    if (e.target.classList.cotainers('show-popup')) {
+        close ();
+    }
+})
